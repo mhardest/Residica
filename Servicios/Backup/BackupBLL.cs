1ab3391aa4148108e6aa4b5f0de13aee0b9fde03
Cristian Matias Martin <CristianMatias.Martin@Alumnos.uai.edu.ar>
2020-07-05 03:27:42 -0300

    ,

---
+++ Servicios/Backup/BackupBLL.cs
@@ -0,0 +1,61 @@
+﻿using Servicios.Excepciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Backup
+{
+    public class BackupBLL
+    {
+        public static void RealizarBackUp(string ruta)
+        {
+            try
+            {
+                BackupDAL.RealizarBackup(ruta);
+            }
+            catch (ExceptionBLL bllex)
+            {
+                throw new ExceptionBLL(bllex, bllex.Message);
+            }
+        }
+
+        public static void RealizarBackUpGestion(string ruta)
+        {
+            try
+            {
+                BackupDAL.RealizarBackupGestion(ruta);
+            }
+            catch (ExceptionBLL bllex)
+            {
+                throw new ExceptionBLL(bllex, bllex.Message);
+            }
+        }
+
+
+        public static void RealizarRestore(string ruta)
+        {
+            try
+            {
+                BackupDAL.RealizarRestore(ruta);
+            }
+            catch (ExceptionBLL bllex)
+            {
+                throw new ExceptionBLL(bllex, bllex.Message);
+            }
+        }
+
+        public static void RealizarRestoreGestion(string ruta)
+        {
+            try
+            {
+                BackupDAL.RealizarRestoreGestion(ruta);
+            }
+            catch (ExceptionBLL bllex)
+            {
+                throw new ExceptionBLL(bllex, bllex.Message);
+            }
+        }
+    }
+}
---
+++ Servicios/Backup/BackupDAL.cs
@@ -0,0 +1,81 @@
+﻿using Servicios.Excepciones;
+using Servicios.External;
+using Servicios.Seguridad.DAL.Sql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Backup
+{
+    public class BackupDAL
+    {
+        public static void RealizarBackup(string ruta)
+        {
+            string spNombre = "Seguridad_BackUp";
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@ruta", DataTypes.ToDBNull(ruta)));
+
+            try
+            {
+                db.EjecutarConsulta(db.TipoBase.ResidicaSeguridad, CommandType.StoredProcedure, spNombre, parametros.ToArray());
+            }
+            catch (SqlException sqlex)
+            {
+                throw new ExceptionDAL(sqlex, sqlex.Message);
+            }
+        }
+
+        public static void RealizarBackupGestion(string ruta)
+        {
+            string spNombre = "Seguridad_BackUpGestion";
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@ruta", DataTypes.ToDBNull(ruta)));
+
+            try
+            {
+                db.EjecutarConsulta(db.TipoBase.ResidicaSeguridad, CommandType.StoredProcedure, spNombre, parametros.ToArray());
+            }
+            catch (SqlException sqlex)
+            {
+                throw new ExceptionDAL(sqlex, sqlex.Message);
+            }
+        }
+
+
+        public static void RealizarRestore(string ruta)
+        {
+            string spNombre = "Seguridad_RestoreBackUp";
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@ruta", DataTypes.ToDBNull(ruta)));
+
+            try
+            {
+                db.EjecutarConsulta(db.TipoBase.ResidicaSeguridad, CommandType.StoredProcedure, spNombre, parametros.ToArray());
+            }
+            catch (SqlException sqlex)
+            {
+                throw new ExceptionDAL(sqlex, sqlex.Message);
+            }
+        }
+
+        public static void RealizarRestoreGestion(string ruta)
+        {
+            string spNombre = "Seguridad_RestoreBackUpGestion";
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@ruta", DataTypes.ToDBNull(ruta)));
+
+            try
+            {
+                db.EjecutarConsulta(db.TipoBase.ResidicaSeguridad, CommandType.StoredProcedure, spNombre, parametros.ToArray());
+            }
+            catch (SqlException sqlex)
+            {
+                throw new ExceptionDAL(sqlex, sqlex.Message);
+            }
+        }
+    }
+}
